1824c59ffbda4493be73b2d7abdf706b1ef9e128
Felix <fs@edgeless.systems>
2020-04-06 14:51:17 +0200

    cleanup

--- bin/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/felix/dev/libedgeless
-# Build directory: /home/felix/dev/libedgeless/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/felix/dev/libedgeless/build/crypto_test[1]_include.cmake")
-subdirs("googletest")
