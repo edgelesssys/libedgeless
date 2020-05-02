d3cfbb8fb38f2f376439e28300561beff649c888
Felix <fs@edgeless.systems>
2020-05-02 10:38:29 +0200

    add conversion

--- include/edgeless/buffer.h
+++ include/edgeless/buffer.h
@@ -6,4 +6,14 @@ namespace edgeless {
 using Buffer = tcb::span<uint8_t>;
 using CBuffer = tcb::span<const uint8_t>;
 
+template<typename T>
+Buffer ToBuffer(T& v) {
+  return {reinterpret_cast<uint8_t*>(&v), sizeof(v)}
+}
+
+template<typename T>
+CBuffer ToCBuffer(const T& v) {
+  return {reinterpret_cast<const uint8_t*>(&v), sizeof(v)}
+}
+
 }  // namespace edgeless
